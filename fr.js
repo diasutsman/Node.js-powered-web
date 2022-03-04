ec29a7539c0c03c3dc91e8690c8213dc092d5e52
diasdev15 <utsmand91@gmail.com>
2022-03-04 10:00:13 +0700

    add seperate file response module

--- fr.js
+++ fr.js
@@ -7,17 +7,28 @@ class FileResponse {
     addResponse(url, path, contentType) {
         this.responses[url] = [path, contentType]
     }
-    response(url) {
+    #renderFile(path, contentType) {
         const { res } = this
-        if (!this.responses[url]) {
-            res.writeHead(404, {'Content-Type': 'text/html'})
-            return res.end()
-        }
-        const [path, contentType] = this.responses[url]
         fs.readFile(path, (err, data) => {
             res.writeHead(200, { 'Content-Type': contentType })
             res.end(data)
         })
+    }
+    response(url) {
+        console.log(url)
+        const { res } = this
+        if (this.responses[url]) {
+            const [path, contentType] = this.responses[url]
+            return this.#renderFile(path, contentType)
+        } else if (url.endsWith('.jpg')) {
+            return this.#renderFile(url.slice(1), 'image/jpeg')
+        } else if (url.endsWith('.css')) {
+            return this.#renderFile(url.slice(1), 'text/css')
+        } else if (url.endsWith('.js')) {
+            return this.#renderFile(url.slice(1), 'application/javascript')
+        }
+        res.writeHead(404, { 'Content-Type': 'text/html' })
+        return res.end()
     }
 }
 exports.FileResponse = FileResponse
